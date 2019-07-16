1a2a5c6c3026763825c36e546d4316b19600104e
Sachin Sarath Y Kothandaraman <sacdav.sarath@gmail.com>
2019-07-16 11:28:27 -0400

    skeleton script
    created python script for task1

---
+++ Task1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+################## Sage Bionetworks - Coding Exercise - Task 1 ###################
+# Created by: Sachin Sarath Y Kothandaraman
+# Date: 16-Jul-2019
+##################################################################################
+
+import csv
+import os
+import sys
+import subprocess
+import argparse
+
+file1 = open("Data1.csv")
+file2 = open("Data2.csv")
+file3 = open("Data3.csv")
+#file4 = open("Color.csv")
+
+templist = [file1, file2, file3]
+tempdict = {}
+
+for x in templist:
+    

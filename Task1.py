143088251a4a7e0889d5dfac9fdfa205ae85fd74
Sachinyadav55 <sacdav.sarath@gmail.com>
2019-07-22 16:39:54 -0400

    Task1 complete. Task2 draft

--- Task1.py
+++ Task1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ################## Sage Bionetworks - Coding Exercise - Task 1 ###################
 # Created by: Sachin Sarath Y Kothandaraman
@@ -10,15 +10,37 @@ import os
 import sys
 import subprocess
 import argparse
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as mpl
+import glob
 
-file1 = open("Data1.csv")
-file2 = open("Data2.csv")
-file3 = open("Data3.csv")
-#file4 = open("Color.csv")
+DIR = os.path.dirname(__file__)
+file_path = os.path.join(DIR,'Data/')
+data_files = glob.glob(file_path + '/*.csv')
 
-templist = [file1, file2, file3]
-tempdict = {}
+li = []
 
-#for x in templist:
+for file in data_files:
+	df = pd.read_csv(file, index_col=None, header=0)
+	li.append(df)
+
+frame1 = pd.concat(li, axis=0, ignore_index=True)
+
+frame2 = pd.read_csv("Color.csv", index_col=None, header=0)
+
+merge_frames = pd.merge(frame1, frame2, on='SampleID', how='outer')
+
+to_print = merge_frames[['SampleID', 'Color', 'X', ('Y')]]
+
+to_print['X'] = pd.to_numeric(to_print['X'], errors='coerce')
+to_print['Y'] = pd.to_numeric(to_print['Y'], errors='coerce')
+
+export_csv = to_print.to_csv('Output.csv', index = None)
+
+to_print['ln(Y)'] = np.log(to_print['Y'])
+
+
+plt = to_print.plot.scatter(x = 'X', y = 'ln(Y)')
+
+mpl.savefig('Scatter_plot.pdf')
